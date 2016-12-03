d0ff565be07587f7e67168b92e9ec3593f14c161
Neil Millard <neil.millard@gmail.com>
2016-12-03 17:03:15 +0000

    base config

---
+++ puppet/modules/role/manifests/webhost.pp
@@ -0,0 +1,12 @@
+class role::webhost {
+  include ::profile::base
+  include ::profile::os_limits
+
+  stage { 'swapfile':
+    before => Stage['main'],
+  }
+
+  class { '::profile::swapfile':
+    stage => swapfile
+  }
+}
